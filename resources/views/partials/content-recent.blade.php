478376e0a9a2c0ba76bb2e947125102acb04cd9c
Stephen Dickinson <stephen@Stephens-MacBook-Pro.local>
2017-10-04 15:05:47 -0600

    Temp styling for posts on front page

--- resources/views/partials/content-recent.blade.php
+++ resources/views/partials/content-recent.blade.php
@@ -1,8 +1,8 @@
 <article @php(post_class('recent'))>
   <header class="entry-header">
-    <h3 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h3>
+    <h3 class="entry-title display-3"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h3>
   </header>
   <div class="entry-content">
-    @php(@empty(get_the_post_thumbnail()) ? the_excerpt() : get_the_post_thumbnail())
+    @php(@empty(get_the_post_thumbnail()) ? the_excerpt() : the_post_thumbnail())
   </div>
 </article>
