3a604fd04b456561c2ef18f2d66919a2aa7de256
Stephen Dickinson <stephencottontail@me.com>
2017-10-06 14:49:39 -0600

    Scroll-and-reveal footer only used on large screens

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,14 @@
 <footer class="content-info">
   <div class="container">
     <div class="row">
-      <div class="col col-md-8">
+      <div class="col-12 col-md-8 mb-5">
         @php(dynamic_sidebar('footer-1'))
       </div>
-      <div class="col col-md-4">
+      <div class="col-12 col-md-4 mb-5">
         @php(dynamic_sidebar('footer-2'))
       </div>
     </div>
-    <div class="site-info py-3 mt-5">
+    <div class="site-info py-3">
       <p class="m-0"><a href="https://wordpress.org/">Proudly powered by WordPress</a> | Copyright &copy; 2017 Stephen Dickinson</p>
     </div>
   </div>
