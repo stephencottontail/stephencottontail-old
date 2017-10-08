a68726094b666780e094e43baec47a5cc4a492ac
Stephen Dickinson <stephencottontail@me.com>
2017-10-07 21:11:21 -0600

    Rename recent posts section in CSS

--- resources/views/partials/content-recent.blade.php
+++ resources/views/partials/content-recent.blade.php
@@ -1,4 +1,4 @@
-<article @php(post_class('recent'))>
+<article @php(post_class(['recent', 'front-post']))>
   <header class="entry-header">
     <h3 class="entry-title display-3"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h3>
   </header>
--- resources/views/partials/recent.blade.php
+++ resources/views/partials/recent.blade.php
@@ -1,6 +1,6 @@
-<section class="recent-posts">
+<section class="front-page-section recent-posts">
   <div class="container">
-    <h2 class="section-title">Recent Posts</h2>
+    <h2 class="section-title">Recent Posts<small class="mx-3"><a href="{{ get_option('page_for_posts') }}">See More&hellip;</a></small></h2>
     @php($args=['posts_per_page'=>4, 'ignore_sticky_posts'=>true])
     @query($args)
       @include('partials.content-recent')
