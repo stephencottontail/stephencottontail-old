6faa75341ff718bcc84f3206f9a40c5498c854d5
Stephen Dickinson <stephencottontail@me.com>
2017-10-13 21:32:21 -0600

    Use a custom function to display comments

---
+++ app/theme.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+function display_comments($comment, $args, $depth) {
+    $GLOBALS['comment'] = $comment;
+    global $post;
+
+    if ('pingback' == $comment->comment_type || 'trackback' == $comment->comment_type) : ?>
+        <li id="div-comment-<?php echo comment_ID(); ?>" <?php comment_class(); ?>>
+            <article class="pingback-body">
+                <header class="pingback-header">Pingback</header>
+                <div class="pingback-content">
+                    <cite><?php comment_author_link(); ?></cite>
+                    <?php edit_comment_link('Edit', '<span class="pingback-edit">', '</span>'); ?>
+                </div>
+            </article>
+    <?php else : ?>
+        <li id="comment-<?php comment_ID(); ?>" <?php comment_class(empty($args['has_children']) ? '' : 'parent'); ?>>
+            <article class="comment-body">
+                <header class="comment-header">
+                    <?php echo get_avatar($comment, $args['avatar_size']); ?>
+                    <cite class="fn"><?php echo get_comment_author_link(); ?></cite>
+                    <div class="comment-meta">
+                        <a href="<?php echo esc_url(get_comment_link($comment->comment_ID)); ?>" class="comment-time"><time datetime="<?php comment_time('c'); ?>"><?php echo get_comment_date(); ?></time></a>
+                        <?php
+                        comment_reply_link(array_merge($args, [
+                            'add_below' => 'comment',
+                            'depth'     => $depth,
+                            'max_depth' => $args['max_depth'],
+                            'before'    => '<span class="comment-reply">',
+                            'after'     => '</span>'
+                        ]));
+
+                        if (get_edit_post_link()) : ?>
+                            <span class="sep"> / </span>
+                            <?php
+                            edit_post_link('Edit', '<span class="comment-edit">', '</span>');
+
+                        endif;
+                        ?>
+                    </div>
+                </header>
+                <div class="comment-content">
+                    <?php if (!empty($comment->comment_parent)) :
+                        $parent_comment = get_comment($comment->comment_parent);
+                        ?>
+                        <span class="in-reply-to">In reply to <a href="<?php echo esc_url(get_comment_link(get_comment($parent_comment))); ?>"><?php echo esc_html($parent_comment->comment_author); ?></a></span>
+                    <?php
+                    endif;
+
+                    if ('0' == $comment->comment_approved) :
+                    ?>
+                        <span class="awaiting-moderation">Your comment is awaiting moderation.</span>
+                    <?php
+                    endif;
+
+                    comment_text();
+                    ?>
+                </div>
+            </article>
+    <?php endif;
+}
--- resources/views/partials/comments.blade.php
+++ resources/views/partials/comments.blade.php
@@ -6,16 +6,14 @@ if (post_password_required()) {
 
 <section id="comments" class="comments">
   @if (have_comments())
-    <h2>
-      {!! sprintf(_nx('One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sage'), number_format_i18n(get_comments_number()), '<span>' . get_the_title() . '</span>') !!}
-    </h2>
+    <h2>Comments ({{ get_comments_number() }})</h2>
 
     <ol class="comment-list">
-      {!! wp_list_comments(['style' => 'ol', 'short_ping' => true]) !!}
+      {!! wp_list_comments(['style' => 'ol', 'avatar_size' => 64, 'short_ping' => true, 'callback' => 'App\display_comments']) !!}
     </ol>
 
     @if (get_comment_pages_count() > 1 && get_option('page_comments'))
-      <nav>
+      <nav class="comment-navigation">
         <ul class="pager">
           @if (get_previous_comments_link())
             <li class="previous">@php(previous_comments_link(__('&larr; Older comments', 'sage')))</li>
